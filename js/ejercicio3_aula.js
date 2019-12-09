08ce848ee55716e0ff9c63b88c8bbc6d711a3e73
Gabriel Cano <gabrielcano.86@gmail.com>
2019-12-09 17:26:05 -0300

    se corrijen los ejercicios

--- js/ejercicio3_aula.js
+++ js/ejercicio3_aula.js
@@ -14,26 +14,44 @@ var auto={
 }
 
 function ingresar_auto(){
+var arranque;
+var text_estado;
+    auto.color=document.getElementById('color').value;
+    auto.marca=document.getElementById('marca').value;
+     auto.modelo=document.getElementById('modelo').value;
+     arranque= document.getElementById('encendido').value;
 
-    auto.color=prompt('ingrese color del auto: ');
-    auto.marca=prompt('ingrese la marca del auto: ');
-    auto.modelo=prompt('ingresar el modelo del auto: ');
-    auto.encendido= encendido()
-    
+     if(arranque==0){
+auto.encendido=true;
+text_estado='ENCENDIDO';
+     }
+     else{
+        auto.encendido=false; 
+        text_estado='APAGADO'; 
+     }
+    //  console.log(arranque)
+
+if (confirm('color: '+auto.color+'\n'+'Marca: '+auto.marca+'\n'+'Modelo: '+auto.modelo+'\n'+'Estado del Auto: '+text_estado)==true){
+    limpiar();
+} 
     }
-ingresar_auto();
+
     
+function limpiar(){
+    document.getElementById('color').value=0;
+    document.getElementById('marca').value=0;
+    document.getElementById('modelo').value=0;
 
-function encendido(){
-    var enciende=prompt('para encender el auto ingrese un valor mayor a 0:');
-
-    if(enciende>0){
-        return true;
-    }
-    return false
 }
+// function encendido(){
+//     var enciende=prompt('para encender el auto ingrese un valor mayor a 0:');
+
+//     if(enciende>0){
+//         return true;
+//     }
+//     return false
+// }
 
 
-console.log(auto);
 
 
